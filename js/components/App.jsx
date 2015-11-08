53349d09bb2f3952f9cc7acc7212e24f18e74282
Noel Tock <hello@noeltock.com>
2015-11-09 08:51:35 +1000

    API request design

--- js/components/App.jsx
+++ js/components/App.jsx
@@ -15,6 +15,18 @@ module.exports = React.createClass({
 			<div className="App">
 				<Menu currentPath={this.props.location.pathname} menu={this.props.menu} onClick={this.handleClickMenuItem} />
 				<div className="page-content">
+					<div className="api-display-wrap">
+						<div className="api-display">
+							The WordPress REST API is the next generation of WordPress, opening the platform up in brand new ways. With the WordPress REST API you can build applications that retrieve information from WordPress, without touching a line of PHP. A Day of REST is a day dedicated to the WordPress REST API. Find out about the possibilities of the WordPress REST API from the people making it, and from the people using it.
+							The WordPress REST API is the next generation of WordPress, opening the platform up in brand new ways. With the WordPress REST API you can build applications that retrieve information from WordPress, without touching a line of PHP. A Day of REST is a day dedicated to the WordPress REST API. Find out about the possibilities of the WordPress REST API from the people making it, and from the people using it.
+							The WordPress REST API is the next generation of WordPress, opening the platform up in brand new ways. With the WordPress REST API you can build applications that retrieve information from WordPress, without touching a line of PHP. A Day of REST is a day dedicated to the WordPress REST API. Find out about the possibilities of the WordPress REST API from the people making it, and from the people using it.
+							The WordPress REST API is the next generation of WordPress, opening the platform up in brand new ways. With the WordPress REST API you can build applications that retrieve information from WordPress, without touching a line of PHP. A Day of REST is a day dedicated to the WordPress REST API. Find out about the possibilities of the WordPress REST API from the people making it, and from the people using it.
+							The WordPress REST API is the next generation of WordPress, opening the platform up in brand new ways. With the WordPress REST API you can build applications that retrieve information from WordPress, without touching a line of PHP. A Day of REST is a day dedicated to the WordPress REST API. Find out about the possibilities of the WordPress REST API from the people making it, and from the people using it.
+							The WordPress REST API is the next generation of WordPress, opening the platform up in brand new ways. With the WordPress REST API you can build applications that retrieve information from WordPress, without touching a line of PHP. A Day of REST is a day dedicated to the WordPress REST API. Find out about the possibilities of the WordPress REST API from the people making it, and from the people using it.
+							The WordPress REST API is the next generation of WordPress, opening the platform up in brand new ways. With the WordPress REST API you can build applications that retrieve information from WordPress, without touching a line of PHP. A Day of REST is a day dedicated to the WordPress REST API. Find out about the possibilities of the WordPress REST API from the people making it, and from the people using it.
+							The WordPress REST API is the next generation of WordPress, opening the platform up in brand new ways. With the WordPress REST API you can build applications that retrieve information from WordPress, without touching a line of PHP. A Day of REST is a day dedicated to the WordPress REST API. Find out about the possibilities of the WordPress REST API from the people making it, and from the people using it.
+						</div>
+					</div>
 					{this.props.children || <Intro />}
 				</div>
 				<APIConsole />
